53109ffafb01b5c4d0fa341f854235529f653e15
HYNIX-Jang <hynix_1@hynixlabs.com>
2020-05-04 16:22:21 +0900

    Change name of Project

--- src/main/java/com/hynixlabs/chart/MusicChartApplication.java
+++ src/main/java/com/hynixlabs/chart/MusicChartApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChartbotApplication {
+public class MusicChartApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChartbotApplication.class, args);
+        SpringApplication.run(MusicChartApplication.class, args);
     }
 
 }
